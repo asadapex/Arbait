5368a111f3322417e4f02418e49ed6b64610d9f3
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-05-26 10:16:31 +0500

    multer get

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -16,8 +18,13 @@ async function bootstrap() {
     .addBearerAuth()
     .addTag('Service')
     .build();
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/file/',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
